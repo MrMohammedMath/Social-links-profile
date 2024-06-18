8d043c3d3fa136c0bb84665b43bc194a596dcb65
mr.math2024 <mr.math2024@gmail.com>
2024-06-18 11:59:12 +0000

    init

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+export default function App() {
+
+  return (
+    <>
+      <main className="bg-[#1F1F1F] my-24  mx-5 p-5 md:p-10 rounded-lg font-Inter md:max-w-md md:mx-auto ">
+        <img src="avatar-jessica.jpeg" alt="jessica" className="rounded-full w-24 md:w-28 mx-auto"/>
+        
+        <h1 className="text-center text-white font-semibold mt-4 md:mt-7 text-xl md:text-2xl">Jessica Randall</h1>
+        <p className="text-[#CDF342] text-center text-xs md:text-sm mt-2 md:mt-3 font-semibold">London, United Kingdom</p>
+        <h2 className="text-center text-gray-100 text-xs md:text-sm mt-5 md:mt-7">"Front-end developer and avid reader."</h2>
+
+        <a href="" className="bg-[#333333] block mt-4 md:mt-8 text-center text-white py-3 rounded-lg font-semibold text-xs md:text-sm ">GitHub</a>
+        <a href="" className="bg-[#333333] block mt-3 md:mt-5 text-center text-white py-3 rounded-lg font-semibold text-xs md:text-sm ">Frontend Mentor</a>
+        <a href="" className="bg-[#333333] block mt-3 md:mt-5 text-center text-white py-3 rounded-lg font-semibold text-xs md:text-sm ">LinkedIn</a>
+        <a href="" className="bg-[#333333] block mt-3 md:mt-5 text-center text-white py-3 rounded-lg font-semibold text-xs md:text-sm ">Twitter</a>
+        <a href="" className="bg-[#333333] block mt-3 md:mt-5 text-center text-white py-3 rounded-lg font-semibold text-xs md:text-sm ">Instagram</a>
+      </main>
+    </>
+  )
+}
